e3fe3910f7dd54c63c35d799135f649253d2e356
Israel <israel.nicacio@outlook.com>
2020-02-19 09:55:26 -0300

    Adicionando componentes header, content e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { MatDialogModule } from '@angular/material/dialog'
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
